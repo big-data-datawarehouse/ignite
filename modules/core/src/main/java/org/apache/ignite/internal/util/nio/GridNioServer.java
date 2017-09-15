4c61ecc7b37785faa00bd52e6eafdd7ceed27196
sboikov <sboikov@gridgain.com>
2017-09-15 16:20:19 +0300

    ignite-6149

--- modules/core/src/main/java/org/apache/ignite/internal/util/nio/GridNioServer.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/nio/GridNioServer.java
@@ -55,6 +55,7 @@ import org.apache.ignite.configuration.ConnectorConfiguration;
 import org.apache.ignite.internal.IgniteInternalFuture;
 import org.apache.ignite.internal.IgniteInterruptedCheckedException;
 import org.apache.ignite.internal.managers.communication.GridIoMessage;
+import org.apache.ignite.internal.processors.cache.mvcc.MvccCoordinatorMessage;
 import org.apache.ignite.internal.util.GridConcurrentHashSet;
 import org.apache.ignite.internal.util.future.GridCompoundFuture;
 import org.apache.ignite.internal.util.nio.ssl.GridNioSslFilter;
@@ -1788,8 +1789,18 @@ public class GridNioServer<T> {
         public void offer(SessionChangeRequest req) {
             changeReqs.offer(req);
 
-            if (select)
+            if (select) {
+                Object msg = req.message();
+
+                if (msg instanceof GridIoMessage && ((GridIoMessage) msg).message() instanceof MvccCoordinatorMessage) {
+                    MvccCoordinatorMessage msg0 = (MvccCoordinatorMessage)((GridIoMessage) msg).message();
+
+                    if (msg0.processedFromNioThread())
+                        return;
+                }
+
                 selector.wakeup();
+            }
         }
 
         /** {@inheritDoc} */
@@ -4096,5 +4107,7 @@ public class GridNioServer<T> {
          * @return Requested change operation.
          */
         NioOperation operation();
+
+        Object message();
     }
 }
